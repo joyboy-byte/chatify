b748bbdf99f2c11f15174e0553c5f8b39c591a22
joyboy-byte <vyash5491@gmail.com>
2026-01-29 23:37:09 +0530

    put the missing null check for seletedUser

--- frontend/src/components/ChatHeader.jsx
+++ frontend/src/components/ChatHeader.jsx
@@ -33,7 +33,7 @@ function ChatHeader() {
       </div>
 
       <button onClick={() => setSelectedUser(null)} >
-        <XIcon claaName='w-5 h-5 text-slate-400 hover:text-slate-200 transition-colors cursor-pointer ' />
+        <XIcon className='w-5 h-5 text-slate-400 hover:text-slate-200 transition-colors cursor-pointer ' />
 
       </button>
     </div>
